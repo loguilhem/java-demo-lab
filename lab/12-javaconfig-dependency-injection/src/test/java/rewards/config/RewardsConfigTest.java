6e91453ed658fc8dbf27a4c829ac77d7db8f60d0
loguilhem <guilhem.vilarroya@protonmail.com>
2024-06-28 11:13:05 +0200

    10+12 tests

---
+++ lab/12-javaconfig-dependency-injection/src/test/java/rewards/config/RewardsConfigTest.java
@@ -0,0 +1,16 @@
+package rewards.config;
+
+import config.RewardsConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import rewards.RewardNetwork;
+import rewards.TestInfrastructureConfig;
+
+import javax.sql.DataSource;
+
+public class RewardsConfigTest {
+
+}
